b3d5027361d6ffa7bd9abefad439d0d58d2842bf
= <stefan.nikolic@sap.com>
2019-11-19 10:13:05 +0100

    Updates

---
+++ precache-manifest.2dcb4d2295bef5ca4c5f41e145498c03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96020568397793ade6bb14fb3a6da92",
+    "url": "/safe-deposit/index.html"
+  },
+  {
+    "revision": "6f5197a4bedfb18166a5",
+    "url": "/safe-deposit/static/css/main.f551923b.chunk.css"
+  },
+  {
+    "revision": "13e01ee9cbbcfeb99c78",
+    "url": "/safe-deposit/static/js/2.66ae90ea.chunk.js"
+  },
+  {
+    "revision": "6f5197a4bedfb18166a5",
+    "url": "/safe-deposit/static/js/main.00446b2a.chunk.js"
+  },
+  {
+    "revision": "f801884fa8d9fd10f856",
+    "url": "/safe-deposit/static/js/runtime-main.83555420.js"
+  },
+  {
+    "revision": "111a056bd351b4b872c577b3729ac264",
+    "url": "/safe-deposit/static/media/button.111a056b.mp3"
+  },
+  {
+    "revision": "dd18d3ba6fe44f11212f40b6132094fe",
+    "url": "/safe-deposit/static/media/error.dd18d3ba.mp3"
+  },
+  {
+    "revision": "a9294e7bf158189760407ff8ea56dba8",
+    "url": "/safe-deposit/static/media/unlock.a9294e7b.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/safedeposit/precache-manifest.15b4f4ae3e17cfc8f53ece11a56d1325.js"
+  "/safe-deposit/precache-manifest.2dcb4d2295bef5ca4c5f41e145498c03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/safedeposit/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/safe-deposit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
